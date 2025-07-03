9e4db90208b2923d51c7731ac195e3c1f24e84fc
Robert Crossland <robertcrossland@gmail.com>
2025-07-02 22:38:51 -0500

    fixed license

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
